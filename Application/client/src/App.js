2800c656748b202938004ff8380f94295003a4af
Luis Gonzalez <otfgonzalez@gmail.com>
2022-09-27 16:44:15 -0700

    Notes added to Application/client/src/app.js

--- Application/client/src/App.js
+++ Application/client/src/App.js
@@ -4,6 +4,26 @@ import Footer from './components/FooterComponent/Footer';
 import Navbar from './components/NavbarComponent/Navbar';
 import Homepage from './components/HomepageComponent/Homepage';
 
+
+/* Folder structure is divided as follows: client (Front end) and server (Backend) */
+/* We might not need a backend so ignore for now. */
+
+/*  Note the way we create components and add them to our this page APP.  */
+/*  Generally, this is the way we can create and work on components seperatly. */
+/*  To add new component, create file with TitleComponent and inside create a page called Title.js inside each component we can also add CSS page.*/
+
+
+/*
+Packages added so far: (NPM)
+-@mui/material 
+-@emotion/react 
+-@emotion/styled
+-react-bootstrap bootstrap
+
+*/
+
+
+
 function App() {
   return (
     <>
