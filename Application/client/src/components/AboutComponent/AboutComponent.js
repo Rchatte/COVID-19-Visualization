16bcb5149a2f40e2fe7ff55b992bc05d889fb757
CharlieB2 <carlos.115.hernandez@gmail.com>
2023-02-28 14:28:35 -0800

    Base Layout Setup
    Still need to insert a carousel of images and descriptions of group members. About our sponsors and gathered sources needs information input.

--- Application/client/src/components/AboutComponent/AboutComponent.js
+++ Application/client/src/components/AboutComponent/AboutComponent.js
@@ -8,7 +8,38 @@ export default function AboutComponent(props) {
         <>
         <Box sx={{ flexGrow: 1 }}>
             <Container>
-                <Typography variant="subtitle1">About Page</Typography>
+                <Typography variant="h3" textAlign={"center"} paddingTop>About Us:</Typography>
+                <Typography variant="h6" textAlign={"center"}>We are a group 
+                consisting of undergraduate, senior-level students 
+                whose primary goal is to observe all aspects of COVID-19 
+                cases to discover hidden correlations between 
+                medical/non-medical factors and their severity.</Typography>
+                
+                <Typography variant="h4" paddingTop>Tableau Information:</Typography>
+                <Typography variant="h6">The purpose of the Tableau 
+                Portal is to create multiple, simply designed, visualizations and experiment 
+                with them to help determine which aspects of COVID have hidden, relevant, 
+                correlations that we can display in chart forms on our JavaScript Application.</Typography>
+                
+                <Typography variant="h4" paddingTop textAlign={"right"}>JavaScript Application Information:</Typography>
+                <Typography variant="h6" textAlign={"right"}>A JavaScript Application was implemented 
+                to acquire greater efficiency when traversing our data with more straightforward navigation 
+                and increased interactivity with each correlation chart. This application is designed to 
+                be used alongside the Tableau Portal because of some of the drawbacks that Tableau has, 
+                such as the low accessibility and high data usage that Tableau requires.</Typography>
+
+                <Typography variant="h4" paddingTop>Machine Learning Information:</Typography>
+                <Typography variant="h6">Machine Learning was used to primarily focus on patients' comorbidity 
+                and genetic factors in relation to their illness and severity of COVID-19. (I believe there was 
+                a discussion of using logistic regression to study these correlations so a note is left here 
+                to include later on) (Also include use of Boruta algorithm as being the main method of use)</Typography>
+
+                <Typography variant="h4" paddingTop textAlign={"center"}>About Our Sponsors:</Typography>
+                <Typography variant="h6" textAlign={"center"}>(Fill in)</Typography>
+
+                <Typography variant="h4" paddingTop textAlign={"center"}>Gathered Sources:</Typography>
+                <Typography variant="h6" textAlign={"center"}>(Fill in)</Typography>
+
             </Container>
         </Box>
         </>
